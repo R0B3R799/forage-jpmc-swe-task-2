a35b9e5e8b4fa6a4fd3f1c84382845bee23215df
R0B3R799 <52356169+R0B3R799@users.noreply.github.com>
2023-08-30 10:40:53 +0100

    This ticket is done when the graph displayed in the client-side web application is a continuously updating line graph whose y-axis is the stock’s top_ask_price and the x-axis is the timestamp of the stock. The continuous updates to the graph should be the result of continuous requests and responses to and from the server for the stock data. This ticket is done when the graph is also able to aggregate duplicated data retrieved from the server

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -25,6 +25,7 @@ interface PerspectiveViewerElement {
 class Graph extends Component<IProps, {}> {
   // Perspective table
   table: Table | undefined;
+  uniqueEntries = new Set();
 
   render() {
     return React.createElement('perspective-viewer');
@@ -32,7 +33,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -47,7 +48,20 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
 
+
+
+
       // Add more Perspective configurations here.
+      elem.setAttribute('view', 'y_line');
+      elem.setAttribute('column-pivots', '["stock"]');
+      elem.setAttribute('row-pivots', '["timestamp"]');
+      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('aggregates', `
+      {"stock": "distinct count",
+      "top_ask_price": "avg",
+      "top_bid_price": "avg",
+      "timestamp": "distinct count"}`);
+
       elem.load(this.table);
     }
   }
@@ -57,7 +71,16 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // As part of the task, you need to fix the way we update the data props to
       // avoid inserting duplicated entries into Perspective table again.
-      this.table.update(this.props.data.map((el: any) => {
+      console.log(this.props.data);
+      const uniqueData = this.props.data.filter((el: any) => {
+        const isDuplicate = this.uniqueEntries.has(el);
+        if (!isDuplicate) {
+          this.uniqueEntries.add(el);
+        }
+        return !isDuplicate;
+      });
+      const formattedData = uniqueData.map((el: any) => {
+        console.log(el);
         // Format the data from ServerRespond to the schema
         return {
           stock: el.stock,
@@ -65,9 +88,10 @@ class Graph extends Component<IProps, {}> {
           top_bid_price: el.top_bid && el.top_bid.price || 0,
           timestamp: el.timestamp,
         };
-      }));
+      });
+      this.table.update(formattedData);
     }
+    console.log(this.uniqueEntries);
   }
 }
-
 export default Graph;
